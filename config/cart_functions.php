e0da74693c596cf9430284f7bae7f4c885ad7447
tejus07 <tejussahi07@gmail.com>
2019-11-29 06:36:12 +0530

    cart_functions updated

--- config/cart_functions.php
+++ config/cart_functions.php
@@ -1,11 +1,5 @@
 <?php
 
-	/*
-		loop through array of $_SESSION['cart'][book_isbn] => number
-		get isbn => take from database => take book price
-		price * number (quantity)
-		return sum of price
-	*/
 	function total_price($cart){
 		$price = 0.0;
 		if(is_array($cart)){
@@ -19,11 +13,7 @@
 		return $price;
 	}
 
-	/*
-		loop through array of $_SESSION['cart'][book_isbn] => number
-		$_SESSION['cart'] is associative array which is [book_isbn] => number of books for each book_isbn
-		calculate sum of books 
-	*/
+	
 	function total_items($cart){
 		$items = 0;
 		if(is_array($cart)){
